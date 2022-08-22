fecc0a132a5875d328027e2b3ed062311ac7ef0d
Aleksey Saharov <lexa-saharov@yandex.ru>
2022-08-23 01:15:26 +0500

    add: error handler

--- controllers/users.js
+++ controllers/users.js
@@ -13,36 +13,66 @@ const User = require('../models/user');
 //   const ERROR_CODE = 400;
 // if (err.name === 'SomeErrorName') return res.status(ERROR_CODE).send(...)
 
-// Ответ с ошибкой должен быть единообразен, как и другие штуки в серверной разработке.
-// Используйте такой формат ответа с ошибкой:
-// {
-//   "message": "Запрашиваемый пользователь не найден"
-// }
+class ApplicationError extends Error {
+  constructor(status = 500, message = 'Internal Error') {
+    super();
+    this.status = status;
+    this.message = message;
+    this.name = this.constructor.name;
+
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+class UserNotFound extends ApplicationError {
+  constructor() {
+    super(404, 'User not found');
+  }
+}
 
 const createUser = (req, res) => User.create(req.body)
   .then((user) => res.status(201).send({ user }))
-  .catch((error) => res.status(500).send({ message: `Error creating user ${error}` }));
+  .catch((error) => {
+    if (error.name === 'ValidationError') {
+      res.status(400).send({ message: `Error validation error ${error}` });
+    } else {
+      res.status(500).send({ message: `Internal server error ${error}` });
+    }
+  });
 
 const getUser = (req, res) => User.findById(req.params.userId)
+  .orFail(() => {
+    console.log('1');
+    throw new UserNotFound();
+  })
   .then((user) => res.status(200).send(user))
-  .catch((error) => {});
+  .catch((error) => {
+    console.log(req.params.userId);
+    if (error.name === 'UserNotFound') {
+      res.status(error.status).send({ message: 'Запрашиваемый пользователь не найден' });
+    } else {
+      res.status(500).send({ message: `Internal server error ${error}` });
+    }
+  });
 
 const getUsers = (req, res) => User.find({})
   .then((users) => res.status(200).send(users))
-  .catch((error) => {});
+  .catch((error) => {
+    res.status(500).send({ message: `Internal server error ${error}` });
+  });
 
 const updateUser = (req, res) => {
   const { name, about } = req.body;
-  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true })
+  User.findByIdAndUpdate(req.user.userId, { name, about }, { new: true })
     .then((user) => res.send(user))
     .catch((error) => res.status(500).send({ message: 'Произошла ошибка' }));
 };
 
 const updateUserAvatar = (req, res) => {
   const { avatar } = req.body;
-  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true })
+  User.findByIdAndUpdate(req.user.userId, { avatar }, { new: true })
     .then((user) => res.send(user))
-    .catch((err) => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch((error) => res.status(500).send({ message: 'Произошла ошибка' }));
 };
 
 module.exports = {
