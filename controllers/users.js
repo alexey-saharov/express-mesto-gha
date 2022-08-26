d80f3ac259b6c7d280171a8c65c0b9faa10fe30b
Aleksey Saharov <lexa-saharov@yandex.ru>
2022-08-26 22:40:46 +0500

    add: createUser, login

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,23 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 const { UserNotFound } = require('../errors/userNotFound');
 const CODE = require('../utils/constants');
+
+const login = (req, res) => {
+  const { email, password } = req.body;
+
+  return User.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
+      res.send({ token });
+    })
+    .catch((err) => {
+      res
+        .status(401)
+        .send({ message: err.message });
+    });
+};
 
 const createUser = (req, res) => {
   const {
@@ -10,21 +27,23 @@ const createUser = (req, res) => {
     email,
     password,
   } = req.body;
-  User.create({
-    name,
-    about,
-    avatar,
-    email,
-    password,
-  })
-    .then((user) => res.status(CODE.SUCCESS_CREATED).send({ user }))
-    .catch((error) => {
-      if (error.name === 'ValidationError') {
-        res.status(CODE.NOT_VALID_DATA).send({ message: `Error validation error ${error.message}` });
-      } else {
-        res.status(CODE.SERVER_ERROR).send({ message: `Internal server error ${error.message}` });
-      }
-    });
+
+  bcrypt.hash(password, 10)
+    .then((hash) => User.create({
+      name,
+      about,
+      avatar,
+      email,
+      password: hash,
+    })
+      .then((user) => res.status(CODE.SUCCESS_CREATED).send({ user }))
+      .catch((error) => {
+        if (error.name === 'ValidationError') {
+          res.status(CODE.NOT_VALID_DATA).send({ message: `Error validation error ${error.message}` });
+        } else {
+          res.status(CODE.SERVER_ERROR).send({ message: `Internal server error ${error.message}` });
+        }
+      }));
 };
 
 const getUser = (req, res) => User.findById(req.params.userId)
@@ -85,6 +104,7 @@ const updateUserAvatar = (req, res) => {
 };
 
 module.exports = {
+  login,
   createUser,
   getUsers,
   getUser,
